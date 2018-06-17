70842bb570b6ffa8dd20dcc0622a7a7224729cd6
Mike Fikes <mike@fikesfarm.com>
2018-06-17 14:46:21 -0400

    Revisions for Canary

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.5.0"
   :global-vars {*warn-on-reflection* true}
-  :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.7.228" :scope "provided"]
+  :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
+                 [org.clojure/clojurescript ~(or (System/getenv "CANARY_CLOJURESCRIPT_VERSION") "1.10.312") :scope "provided"]
                  [com.yahoo.platform.yui/yuicompressor "2.4.8"
                   :exclusions [rhino/js]]]
   :npm {:dependencies [[source-map-support "0.4.0"]]}
